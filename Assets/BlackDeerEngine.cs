a35ff99abea8310bb56961b2e0ca8887b477b0fd
Chanwoo Noh <rajephon@gmail.com>
2017-09-15 19:27:41 +0900

    wip

--- Assets/BlackDeerEngine.cs
+++ Assets/BlackDeerEngine.cs
@@ -8,10 +8,27 @@ using System.Text;
 using System;
 
 public class BlackDeerEngine : MonoBehaviour {
+	public struct GameProgress {
+		public int scenario;
+		public int episode;
+		public string place;
+		public int scene;
+		public int action;
+		public GameProgress(int scenario, int episode, string place, int scene, int action) {
+			this.scenario = scenario;
+			this.episode = episode;
+			this.place = place;
+			this.scene = scene;
+			this.action = action;
+		}
+	}
 	public string m_strName = "xml/SampleEpisode.xml";
+	private static GameProgress gameProgress;
+	private XmlDocument xmlDoc;
 	// Use this for initialization
 	void Start () {
-		StartCoroutine(Process());
+		gameProgress = new GameProgress(1, 1, "설원", 1, 1);
+		StartCoroutine(LoadXMLResource());
 	}
 	
 	// Update is called once per frame
@@ -19,7 +36,7 @@ public class BlackDeerEngine : MonoBehaviour {
 		
 	}
 
-	IEnumerator Process() {
+	IEnumerator LoadXMLResource() {
 		string strPath = string.Empty;
 		// 플랫폼 처리
 		// #if ( UNITY_EDI)
@@ -42,12 +59,20 @@ public class BlackDeerEngine : MonoBehaviour {
 
 		Debug.Log("Read Content: " + www.text.Length);
 
-		Interpret(www.text);
+		StringReader stringReader = new StringReader(www.text);
+		XmlNodeList xmlNodeList = null;
+		xmlDoc = new XmlDocument();
+		xmlDoc.LoadXml(stringReader.ReadToEnd());
+		// Interpret(www.text);
 		
+	}
+	
+	private void progress() {
+		// TODO: action
 	}
 
 	private void Interpret(string _strSource) {
-		Debug.Log("str: " + _strSource);
+		Debug.Log("Interpret: " + _strSource);
 		StringReader stringReader = new StringReader(_strSource);
 		// stringReader.Read();
 		XmlNodeList xmlNodeList = null;
