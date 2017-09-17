c7ed34afa459fb7ef68006e5041dba701e15bcf6
Chanwoo Noh <rajephon@gmail.com>
2017-09-17 16:07:49 +0900

    wip

--- Assets/BlackDeerEngine.cs
+++ Assets/BlackDeerEngine.cs
@@ -8,18 +8,24 @@ using System.Text;
 using System;
 
 public class BlackDeerEngine : MonoBehaviour {
-	public struct GameProgress {
-		public int scenario;
+	public class GameProgress {
+		public int stage;
 		public int episode;
 		public string place;
 		public int scene;
 		public int action;
-		public GameProgress(int scenario, int episode, string place, int scene, int action) {
-			this.scenario = scenario;
+		public GameProgress(int stage, int episode, string place, int scene, int action) {
+			this.stage = stage;
 			this.episode = episode;
 			this.place = place;
 			this.scene = scene;
 			this.action = action;
+		}
+		public string getXPath() {
+			return getXPath(stage, episode, place, scene, action);
+		}
+		public string getXPath(int stage, int episode, string place, int scene, int action) {
+			return "stage[@value=\""+stage+"\"]/episode[@value = \""+episode+"\"]/place[@name=\""+place+"\"]/scene[@no=\""+scene+"\"]/action["+action+"]";
 		}
 	}
 	public string m_strName = "xml/SampleEpisode.xml";
@@ -63,25 +69,38 @@ public class BlackDeerEngine : MonoBehaviour {
 		xmlDoc = new XmlDocument();
 		xmlDoc.LoadXml(stringReader.ReadToEnd());
 		// Interpret(www.text);
-		
+		progress();
 	}
-	
+	private void printError(string msg) {
+		Debug.Log(msg + " (XPath: "+gameProgress.getXPath()+")");
+	}
 	private void progress() {
 		XmlNodeList xmlNodeList = null;
-		xmlNodeList = xmlDoc.SelectNodes("scenario");
-		foreach(XmlNode node in xmlNodeList) {
-			if (node.Name.Equals("scenario") && node.HasChildNodes) {
-				Debug.Log("222222");
-				foreach(XmlNode child in node.ChildNodes) {
-					Debug.Log("333333");
-					Debug.Log("id: "+ child.Name);
-					if (child.Name == "episode") {
-						
-					}
-					// Debug.Log("id: " + child.Attributes.GetNamedItem("id").Value);
-				}
-			}
+		Debug.Log("getXPath: "+ gameProgress.getXPath());
+		xmlNodeList = xmlDoc.SelectNodes(gameProgress.getXPath());
+		
+		if (xmlNodeList.Count == 0) {
+			printError("ERROR: xmlNodeList.Count == 0");
+			return;
 		}
+		Debug.Log("A c: " + xmlNodeList.Count);
+		XmlNode actionNode = xmlNodeList[0];
+		if (actionNode.Attributes["name"] == null) {
+			printError("ERROR: actionNode.Attributes[\"name\"] is null");
+			return;
+		}
+		BDAction bdAction = new BDAction(actionNode.Attributes["name"].Value);
+
+		// foreach(XmlNode node in xmlNodeList) {
+		// 	if (node.Name.Equals("stage") && node.HasChildNodes) {
+		// 		foreach(XmlNode child in node.ChildNodes) {
+		// 			Debug.Log("id: "+ child.Name);
+		// 			if (child.Name == "episode") {
+		// 			}
+		// 			// Debug.Log("id: " + child.Attributes.GetNamedItem("id").Value);
+		// 		}
+		// 	}
+		// }
 	}
 
 }
---
+++ Assets/BlackDeerEngine/Action/BDAction.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BDAction {
+	private string name = "";
+	public BDAction() {
+
+	}
+	public BDAction(string name) {
+		this.name = name;
+	}
+	public string Name {
+		get { return name; }
+		set { this.name = Name; }
+	}
+	
+}
